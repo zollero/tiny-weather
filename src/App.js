6447a30c9dfd32cce23e11b7c419b36a9dd3340b
zollero <corona7@163.com>
2022-04-09 02:26:28 +0800

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css'
 
